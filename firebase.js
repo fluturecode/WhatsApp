61a3aa7a53cda03b6e0cbb876c560989adf8ad87
fluturecode <fluturecode@gmail.com>
2020-08-18 15:03:17 -0400

    latest commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCABaaICtT2wRWlHn9K70aZccYzXijQQhU",
+	authDomain: "whatsapp-b7e51.firebaseapp.com",
+	databaseURL: "https://whatsapp-b7e51.firebaseio.com",
+	projectId: "whatsapp-b7e51",
+	storageBucket: "whatsapp-b7e51.appspot.com",
+	messagingSenderId: "367892779671",
+	appId: "1:367892779671:web:3877ed86e629513cfa8fd2",
+	measurementId: "G-KZV7MW11HM",
+};
--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-	return <div className="app">I am WhatsApp</div>;
+	return (
+		<div className="app">
+			<div className="app__body">
+				<Sidebar />
+				{/* <Chat /> */}
+			</div>
+		</div>
+	);
 }
 
 export default App;
