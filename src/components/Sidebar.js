f947afcbe2d8a1094db316ec8216e782b6790b80
fluturecode <fluturecode@gmail.com>
2020-08-18 15:10:44 -0400

    latest commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Avatar, IconButton } from "@material-ui/core";
-import { DonutLargeIcon } from "@material-ui/icons/DonutLarge";
-import { ChatIcon } from "@material-ui/icons/Chat";
-import { MoreVertIcon } from "@material-ui/icons/MoreVert";
-import from { SearchOutlined}
-
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import from SearchOutlined from '@material-ui/icons/SearchOutlined';
 import "./sidebar.css";
 
 function Sidebar() {
